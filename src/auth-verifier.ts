8bbe3890d54381caf4f6aad87c2908a7c7144272
hakanErgin <hakan.ergin@gmail.com>
2021-01-05 15:14:32 +0100

    wip

--- src/auth-verifier.ts
+++ src/auth-verifier.ts
@@ -5,14 +5,11 @@ const client = new OAuth2Client(CLIENT_ID);
 export default async function verify() {
   const ticket = await client.verifyIdToken({
     idToken: ID_TOKEN,
-    audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
-    // Or, if multiple clients access the backend:
-    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    audience: CLIENT_ID,
   });
   const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
+  const userid = payload.sub;
+
   return userid;
 }
 verify().catch(console.error);
