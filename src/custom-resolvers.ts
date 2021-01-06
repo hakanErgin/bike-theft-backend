7abf69ae14ed47318c98326e5ed239e1e3ed60f5
hakanErgin <hakan.ergin@gmail.com>
2021-01-06 01:58:20 +0100

    cleanup -more to do

--- src/custom-resolvers.ts
+++ src/custom-resolvers.ts
@@ -1,10 +1,6 @@
-import { QueryFilter } from 'graphback';
 import { GraphQLResolveInfo } from 'graphql';
 import { IResolvers } from 'apollo-server-express';
-import { UserFilter } from './generated-types';
 import { GraphQLContext } from './custom-context';
-import { GraphbackContext, GraphbackCRUDService } from 'graphback';
-import { User } from './generated-types';
 import verify from './auth-verifier';
 
 export const userResolvers: IResolvers = {
