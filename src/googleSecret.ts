fbda3892f76213e65bcf4530e13524de273c5705
hakanErgin <hakan.ergin@gmail.com>
2021-04-13 16:24:24 +0200

    jwt added secrets

--- src/googleSecret.ts
+++ src/googleSecret.ts
@@ -1,11 +1,9 @@
-const name = 'projects/119764587873/secrets/backend-env/versions/1';
-// const name = 'projects/my-project/secrets/my-secret/versions/latest';
+const name = 'projects/119764587873/secrets/backend-env/versions/3';
 
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
-const client = new SecretManagerServiceClient();
-
 export default async function getSecrets() {
+  const client = new SecretManagerServiceClient();
   const [version] = await client.accessSecretVersion({
     name: name,
   });
@@ -13,9 +11,14 @@ export default async function getSecrets() {
   // Extract the payload as a string.
   const payload = version.payload.data.toString();
 
-  const [DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST, CLIENT_ID] = payload
-    .split('\r\n')
-    .map((val: any) => val.split('=')[1]);
+  const [
+    DB_USER,
+    DB_PASSWORD,
+    DB_DATABASE,
+    DB_HOST,
+    CLIENT_ID,
+    JWT_SECRET,
+  ] = payload.split('\r\n').map((val: any) => val.split('=')[1]);
 
-  return { DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST, CLIENT_ID };
+  return { DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST, CLIENT_ID, JWT_SECRET };
 }
